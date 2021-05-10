cd0d50be26de1c28f3ec894b221134802476d7e5
AlishaKalra <AlishaKalra@HSSLPT-673>
2021-05-10 13:36:01 +0530

    third Commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,7 +6,11 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
-
+using MediatR;
+using Application.Activities;
+using Application.Core;
+using AutoMapper;
+using API.Extensions;
 
 namespace API
 {
@@ -25,18 +29,7 @@ namespace API
 
             
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>{
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt => {
-                opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
+            services.AddApplicationServices(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
